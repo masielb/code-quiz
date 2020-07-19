03aa32c866053f04a032eb2bd51647a242406d02
Masiel Bautista <masiel5@users.noreply.github.com>
2020-07-19 19:55:46 -0400

    fixed Done page and list styling

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 /* Variables */
+var header = document.querySelector("header");
+var score = document.getElementById("score");
+
+
+
+// Initial Page
+var quizChallengePage = document.getElementById("quizChallengePage");
+var startBtn = document.getElementById("startBtn");
+
+// Questions and Four Answer Choices
+var quizQuestionsPage = document.getElementById("quizQuestionsPage");
+var quizQuestion = document.getElementById("quizQuestion");
+var choice1 = document.getElementById("one");
+var choice2 = document.getElementById("two");
+var choice3 = document.getElementById("three");
+var choice4 = document.getElementById("four");
+var questionBtn = document.querySelector(".questionBtn");
+
+// All Done Page
+var donePage = document.getElementById("donePage");
+var allDone = document.getElementById("allDone");
+var allDoneButtons = document.getElementById("form-control");
+
+var finalScoreIs = document.getElementById("finalScoreIs");
+
+var highScoreButtons = document.getElementById("highScoreButtons");
+
+// var initialButton = document.getElementById("initialButton"); 
+// var initials = document.getElementById("initials"); 
+// var initialInput = document.getElementById("initialInput"); 
+
+// Timer
+var timer = document.getElementById("timer"); 
