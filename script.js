f9dc747dc9ab6f7b81336712aa89939a057eb006
Masiel Bautista <masiel5@users.noreply.github.com>
2020-07-20 21:06:13 -0400

    created variables in js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 var header = document.querySelector("header");
 var score = document.getElementById("score");
 
-
-
 // Initial Page
 var quizChallengePage = document.getElementById("quizChallengePage");
 var startBtn = document.getElementById("startBtn");
@@ -21,14 +19,11 @@ var questionBtn = document.querySelector(".questionBtn");
 var donePage = document.getElementById("donePage");
 var allDone = document.getElementById("allDone");
 var allDoneButtons = document.getElementById("form-control");
-
 var finalScoreIs = document.getElementById("finalScoreIs");
+var initialBtn = document.getElementById("initialBtn"); 
+var initialInput = document.getElementById("initialInput"); 
 
 var highScoreButtons = document.getElementById("highScoreButtons");
-
-// var initialButton = document.getElementById("initialButton"); 
-// var initials = document.getElementById("initials"); 
-// var initialInput = document.getElementById("initialInput"); 
 
 // Timer
 var timer = document.getElementById("timer"); 
